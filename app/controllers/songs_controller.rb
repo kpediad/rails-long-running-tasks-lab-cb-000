21a63ada9f94353f268b0564749d498d5f9bd8b2
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-03 15:21:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
@@ -46,7 +47,10 @@ class SongsController < ApplicationController
   end
 
   def upload
-    
+    CSV.foreach(params[:songs].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+    redirect_to songs_path
   end
 
   private
@@ -55,4 +59,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
